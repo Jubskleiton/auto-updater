e83fea8010caa9fa1b0425398d64470a49b8363a
Jubskleiton <121841208+Jubskleiton@users.noreply.github.com>
2023-01-03 15:17:28 -0300

    check if it needs to update and if so download the new version of the program

---
+++ current.py
@@ -0,0 +1,6 @@
+CURRENT_VERSION = "0.0.0"
+
+ver = []
+for num in CURRENT_VERSION.split("."):
+    ver.append(int(num))
+CURRENT_VERSION_SPLITED = ver
--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from urllib import request
 import json
+import  current
+
+
+def update(url, version):
+    code_name = "demo"
+    extention = ".py"
+    request.urlretrieve(f"{url}{code_name}-{version}{extention}", f"{code_name}-{version}{extention}")
+
+
+def run_program():
+    pass
+
 
 def main():
+    # request and download de info file
     url = 'https://raw.githubusercontent.com/Jubskleiton/auto-updater/main/versions/latest_version.py'
     request.urlretrieve(url, 'latest_version.json')
 
@@ -11,4 +24,15 @@ def main():
         data = json.load(file)
 
     # Now you can access the data in the variable as a Python object
-    print(data['latest'])
+    if data["latest"] != current.CURRENT_VERSION:
+        latest_version_splited = data["latest"].split(".")
+        if int(latest_version_splited[0]) > current.CURRENT_VERSION_SPLITED[0]:
+            print("Major update detected updating now")
+            update(data["url"], data["latest"])
+        elif int(latest_version_splited[1]) > current.CURRENT_VERSION_SPLITED[1]:
+            print("New version detected updating")
+            update(data["url"], data["latest"])
+        elif int(latest_version_splited[2]) > current.CURRENT_VERSION_SPLITED[2]:
+            update(data["url"], data["latest"])
+    else:
+        run_program()
